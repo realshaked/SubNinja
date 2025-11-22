26b6b862c50706920510cd9e7a2677b5532c228b
Davi <102493409+Davi-CP@users.noreply.github.com>
2025-11-22 09:10:05 -0300

    Atualização de segurança em app.js
    Updated MongoDB connection string to an empty value.

--- subninja-backend/subninja/app.js
+++ subninja-backend/subninja/app.js
@@ -10,7 +10,7 @@ var notificacoesRouter = require('./routes/notificacoes');
 
 
 const mongoose = require('mongoose');
-const connectionString = "mongodb+srv://subninja_db_user:ZvyTh6GqTs71gnwp@clustersubninja.dp7bbmw.mongodb.net/?appName=ClusterSubninja"
+const connectionString = ""
 const connect = mongoose.connect(connectionString)
 require('./jobs/notificacoesJob');
 const session = require('express-session');
